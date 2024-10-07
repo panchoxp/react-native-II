c058f5b7a6c14903ee3ee3c297da162f4842ea2d
panchoxp <francisc_smfd@hotmail.es>
2024-10-07 17:56:46 -0500

    first commit

---
+++ src/theme/styles.tsx
@@ -0,0 +1,55 @@
+import { StatusBar, StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+        gap: 10
+    },
+    text: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    rootHome: {
+        flex: 1,
+        alignItems: 'flex-start',
+        padding: 20,        
+    },
+    header: {
+        flexDirection: 'row',
+        gap: 15,
+        alignItems: 'center'
+    },
+    fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+    },
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+    modal:{
+        padding:20,
+        marginHorizontal:20,
+        backgroundColor:'#fff',
+        borderRadius:10,
+        gap:10
+    },
+    icon:{
+        alignItems:'flex-end',
+        flex:1
+    }
+})
